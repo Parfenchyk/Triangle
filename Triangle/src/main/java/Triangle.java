3fe1d482af8a4fd25eab5d918398aedc24f96c9c
Parfenchyk <petrovich.minsk@gmail.com>
2023-09-18 22:01:07 +0300

    Base version 1.3

--- Triangle/src/main/java/Triangle.java
+++ Triangle/src/main/java/Triangle.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -6,13 +7,16 @@ public class Triangle {
         System.out.print("Enter first side: ");
         int a = sc.nextInt();
 
+
         System.out.print("Enter second side: ");
         int b = sc.nextInt();
 
         System.out.print("Enter third side: ");
         int c = sc.nextInt();
-
-        if ((a + b > c && b + c > a && a + c > b) || (b + a > c && a + c > b && b + c > a) || (c + a > b && a + b > c && c + b > a)) {
+        sc.close();
+        if ((a + b > c && b + c > a && a + c > b)
+                || (b + a > c && a + c > b && b + c > a)
+                || (c + a > b && a + b > c && c + b > a)) {
             System.out.println("Triangle is valid.");
 
             if (a == b && b == c) {
